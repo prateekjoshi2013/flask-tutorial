727cf5c239b4e428be767bf661c788b419cbf6bf
Prateek Joshi <prateekjoshi2013@gmail.com>
2022-04-01 21:36:58 -0700

    initial commit

---
+++ bookmarksrestapi/src/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask,jsonify
+
+def create_app(test_config=None):
+    app=Flask(__name__,instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get('SECRET_KEY'),
+        )
+    else:
+        app.config.from_mapping(test_config)
+
+
+    @app.get("/")
+    def index():
+        return "hello world"
+
+    @app.get("/hello")
+    def say_index():
+        return jsonify({'message' :"hello world"})
+
+    return app
