f01692dc0af14bf3e8fe0e0e2451f9c3d45efcb4
Prateek Joshi <prateekjoshi2013@gmail.com>
2022-04-02 13:59:17 -0700

    added delete endpoint

--- bookmarksrestapi/src/bookmarks.py
+++ bookmarksrestapi/src/bookmarks.py
@@ -107,3 +107,15 @@ def  edit_bookmark(id):
             'created_at':bookmark.created_at,
             'updated_at':bookmark.updated_at,
         }),HTTPStatus.OK.value
+
+
+@bookmarks.delete("/<int:id>")
+@jwt_required()
+def delete_bookmark(id):
+    curent_user=get_jwt_identity()
+    bookmark=Bookmark.query.filter_by(user_id=curent_user,id=id).first()
+    if not bookmark:
+        return jsonify({'message':'Item not found'}),HTTPStatus.NOT_FOUND.value
+    db.session.delete(bookmark)
+    db.session.commit()
+    return jsonify({}),HTTPStatus.NO_CONTENT.value
