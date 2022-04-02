f8fc575cc9bd6c4a65d0c82c2c7f122b7313e321
Prateek Joshi <prateekjoshi2013@gmail.com>
2022-04-02 10:46:57 -0700

    Added login using jwt authentication

--- bookmarksrestapi/src/__init__.py
+++ bookmarksrestapi/src/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask,jsonify
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
+from flask_jwt_extended import JWTManager
 
 def create_app(test_config=None):
     app=Flask(__name__,instance_relative_config=True)
@@ -11,7 +12,8 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get('SECRET_KEY'),
             SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DB_URI'),
-            SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+            SQLALCHEMY_TRACK_MODIFICATIONS=os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS'),
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
         )
     else:
         app.config.from_mapping(test_config)
@@ -19,6 +21,8 @@ def create_app(test_config=None):
     db.app=app
     db.init_app(app)
 
+    JWTManager(app)
+    
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
--- bookmarksrestapi/src/auth.py
+++ bookmarksrestapi/src/auth.py
@@ -1,8 +1,10 @@
+from venv import create
 from flask import Blueprint,request,jsonify
 from werkzeug.security import check_password_hash,generate_password_hash
 from http import HTTPStatus
 from src.database import User,db
 import validators
+from flask_jwt_extended import create_access_token,create_refresh_token
 
 auth=Blueprint('auth',__name__,url_prefix="/api/v1/auth")
 
@@ -35,6 +37,23 @@ def register():
     }),HTTPStatus.CREATED.value
 
 
-@auth.get("/me")
-def me():
-    return {'user':'me'}
+@auth.post("/login")
+def login():
+    email=request.json.get('email','')
+    password=request.json.get('password','')
+    user=User.query.filter_by(email=email).first()
+    if user:
+        is_pass_correct=check_password_hash(user.password,password)
+        if is_pass_correct:
+            refresh=create_refresh_token(identity=user.id)
+            access=create_access_token(identity=user.id)
+            return jsonify({
+                'user':{
+                    'refresh':refresh,
+                    'access':access,
+                    'username':user.username,
+                    'email':user.email,
+                }
+            }),HTTPStatus.OK.value
+    return {'error':'wrong credentials'},HTTPStatus.UNAUTHORIZED.value
+
